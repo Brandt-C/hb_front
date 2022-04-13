00be6fd1977b8d953b7dc91895ac3b3f17d958b3
Brandt-C <brandt.carlson11@gmail.com>
2022-04-13 11:27:58 -0500

    new post working!

--- src/views/userhome.js
+++ src/views/userhome.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { DataContext } from '../context/dataprovider';
 
 
 const UserHome = () => {
@@ -10,6 +10,9 @@ const UserHome = () => {
             [evt.target.name]: value
         });
     }
+    // const disableButton = () => {
+    //     buttonRef.current.disabled = true;
+    // }
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -23,9 +26,9 @@ const UserHome = () => {
 
     }
 
-
+    const {user} = useContext(DataContext)
     const [newpost, setNewpost] = React.useState({
-        userid: '',
+        userid: user.userid,
         title: '',
         body: '',
     })
@@ -35,7 +38,7 @@ const UserHome = () => {
                 <h1>Let's build your hood!</h1>
             </div>
             <div className="row justify-content-center">
-                <iframe title='OSMmapframe' width="300" height="400" frameBorder="1" scrolling="no" marginHeight="0" marginWidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-87.77818679809572%2C41.83171182161546%2C-87.55159378051758%2C41.94442556628153&amp;layer=mapnik&amp;marker=41.888093552297654%2C-87.66489028930664" style={{ border: 1 }}></iframe><br /><small><a href="https://www.openstreetmap.org/?mlat=41.8881&amp;mlon=-87.6649#map=13/41.8881/-87.6649">View Larger Map</a></small>
+                <iframe title='OSMmapframe' width="300" height="350" frameBorder="1" scrolling="no" marginHeight="0" marginWidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-87.77818679809572%2C41.83171182161546%2C-87.55159378051758%2C41.94442556628153&amp;layer=mapnik&amp;marker=41.888093552297654%2C-87.66489028930664" style={{ border: 1 }}></iframe><br /><small><a href="https://www.openstreetmap.org/?mlat=41.8881&amp;mlon=-87.6649#map=13/41.8881/-87.6649">View Larger Map</a></small>
             </div>
             <form onSubmit={handleSubmit}>
                 <div className="row justify-content-start">
@@ -44,14 +47,14 @@ const UserHome = () => {
                         <div className="card-body">
                             <h3>New post:</h3>
                             <h5 className="card-title">Title:<input type="text" name="title" value={newpost.title} onChange={handleChange} /></h5>
-                            <p className="card-text">Body (only 250 characters or less please!):<input type="text" name="title" value={newpost.body} onChange={handleChange} /></p>
+                            <p className="card-text">Body (only 250 characters or less please!):<textarea type="text" name="body" value={newpost.body} onChange={handleChange} /></p>
                         </div>
                         <ul className="list-group list-group-flush">
                             <li className="list-group-item">By:</li>
                             <li className="list-group-item">When:</li>
                         </ul>
-                        <button type="submit" value='newpost' className="btn btn-success">Create Post</button>
-                        <button type="" value='newpost' className="btn btn-primary">Pin to map</button>
+                        <button type="submit" value='newpost' className="btn btn-success p-1 mb-1">Create Post</button>
+                        <button type="" value='mappin' className="btn btn-primary p-1">Pin to map</button>
                     </div>
 
                 </div>
